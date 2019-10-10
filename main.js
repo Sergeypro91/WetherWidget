8066a16247524b1542edb17e105e45fc82b6601b
Sergey Shevtsov <sergeyprostoas@gmail.com>
2019-10-10 13:10:56 +0300

    Version 1.1.2

--- main.js
+++ main.js
@@ -21,17 +21,30 @@ $(document).ready(function() {
     var icon = data.weather[0].icon;
     var iconUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
     
-    $('.city__name').html(city);
-    $('.temperature__current').html(temperature);
-    $('.otherParameters__humidity').html(humidity);
-    $('.otherParameters__windSpeed').html(windSpeed);
-    $('.otherParameters__pressure').html(pressure);
-    $('.temperature__img').attr('src', iconUrl);
+    $('.city__name').html(city).addClass('show').removeClass('hide');
+    $('.temperature__current').html(temperature).addClass('show').removeClass('hide');
+    $('.otherParameters__humidity').html(humidity).addClass('show').removeClass('hide');
+    $('.otherParameters__windSpeed').html(windSpeed).addClass('show').removeClass('hide');
+    $('.otherParameters__pressure').html(pressure).addClass('show').removeClass('hide');
+    $('.temperature__img').attr('src', iconUrl).addClass('show').removeClass('hide');
+
+    function hideBlock(addClass) {
+      $(addClass).addClass('hide').removeClass('show');
+    }
+
+    setTimeout(function(){
+      hideBlock('.city__name');
+      hideBlock('.temperature__current');
+      hideBlock('.otherParameters__humidity');
+      hideBlock('.otherParameters__windSpeed');
+      hideBlock('.otherParameters__pressure');
+      hideBlock('.temperature__img');
+    }, 4000);
   }
 
   weather('https://api.openweathermap.org/data/2.5/weather?lat=43.1151&units=metric&lon=131.8856&APPID=027b14fea92c2edbe2c6aca5c7994af5');
 
-  var city = [
+  var citys = [
     'https://api.openweathermap.org/data/2.5/weather?lat=48.7107&units=metric&lon=44.5168&lang=ru&APPID=027b14fea92c2edbe2c6aca5c7994af5',
     'https://api.openweathermap.org/data/2.5/weather?lat=43.1151&units=metric&lon=131.8856&APPID=027b14fea92c2edbe2c6aca5c7994af5'
     ];
@@ -40,13 +53,13 @@ $(document).ready(function() {
 
   setInterval(function(){
     
-    if (i ==! city.length) {
-      weather(city[i]);
+    if (i ==! citys.length) {
+      weather(citys[i]);
       i++;
     } else {
-      weather(city[i]);
+      weather(citys[i]);
       i = 0;
     }
 
-  }, 4000);      
+  }, 4500);      
 });
